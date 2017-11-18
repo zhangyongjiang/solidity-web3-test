c9c5b776843d4042ca18003c8e8452fbb0338bc7
Kevin Zhang <kevin.zhang@bcgdv.com>
2017-11-18 12:30:59 -0800

    init import

---
+++ src/components/Common.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Card, Col, Row } from 'antd';
+
+class Common extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const { payroll, web3 } = this.props;
+    const updateInfo = (error, result) => {
+      if (!error) {
+        this.getInfo();
+      }
+    }
+
+
+    this.getInfo();
+  }
+
+  componentWillUnmount() {
+  }
+
+  getInfo = () => {
+
+  }
+
+  render() {
+    const { runway, balance, employeeCount } = this.state;
+    return (
+      <div>
+        <h2>通用信息</h2>
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card title="合约金额">{balance} Ether</Card>
+          </Col>
+          <Col span={8}>
+            <Card title="员工人数">{employeeCount}</Card>
+          </Col>
+          <Col span={8}>
+            <Card title="可支付次数">{runway}</Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default Common
